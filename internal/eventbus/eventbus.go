7c8bf63218d61cc3cce08755a63d3ab7721cce60
gogogo1024 <jxycbjhc@gmail.com>
2025-03-05 15:18:54 +0800

    add event bus implementation for publishing and consuming events

---
+++ internal/eventbus/eventbus.go
@@ -0,0 +1,20 @@
+package eventbus
+
+import (
+	"defi/internal/config"
+	"defi/internal/model"
+	"log"
+)
+
+type EventBus interface {
+	PublishEvent(topic string, event []byte) error
+	ConsumerEvent(topic string, handler func(event model.Event)) error
+}
+
+func InitEventBus(cfg config.MQConfig) EventBus {
+	mqEventBus, err := NewEventBus(cfg)
+	if err != nil {
+		log.Fatalf("Failed to create EventBus: %v", err)
+	}
+	return mqEventBus
+}
